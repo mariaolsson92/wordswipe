16e4776d0b32fa9ef904b7e95a77eef2a0dd5aab
maria <maria.olsson92@hotmail.se>
2021-06-29 19:10:56 +0200

    Restarted project

--- app/src/main/java/com/example/wordswipe/MainActivity.kt
+++ app/src/main/java/com/example/wordswipe/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.wordswipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wordswipe.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
